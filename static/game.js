4b71fc089dc8dfb6f86b344f8917023cd2558bad
gloss-water <gloss.water@gmail.com>
2018-04-20 20:18:06 -0600

    update to class based, add some sprites

--- static/game.js
+++ static/game.js
@@ -1,9 +1,89 @@
 
 
+        var player;
+        var cursors;
+class InitialScene extends Phaser.Scene {
+    constructor() {
+        super('initialScene');
+    }
+    preload() {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('shack', 'assets/bigshq.png');
+        this.load.image('door', 'assets/door.png', {
+            frameWidth: 32,
+            frameHeight: 32
+        });
+        this.load.spritesheet('touhou', 'assets/abigail.png', {
+            frameWidth: 16,
+            frameHeight: 31
+        });
+    }
+    create() {
+        this.add.image(400, 300, 'sky');
+        this.add.image(400, 580, 'ground').setScale(2);
+
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 625, 'ground').setScale(2).refreshBody();
+
+        this.add.image(600, 470, 'shack');
+
+        this.doors = this.physics.add.staticGroup();
+        this.doors.create(578, 525, 'door')
+
+
+        this.player = this.physics.add.sprite(100, 450, 'touhou').setScale(2.5);
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.player.body.setGravityY(300);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.anims.create({
+            key: 'neutral',
+            frames: [ { key: 'touhou', frame: 0 }],
+            frameRate: 20
+        });
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('touhou', { start: 4, end: 7 }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('touhou', { start: 12, end: 15 }),
+            frameRate: 10,
+            repeat: -1
+        })
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-150);
+            this.player.anims.play('left', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(150);
+            this.player.anims.play('right', true);
+        } else {
+            this.player.setVelocityX(0);
+            this.player.anims.play('neutral', true);
+        }
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-250, true);
+        }
+    }
+}
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scene: [
+        InitialScene
+    ],
     physics: {
         default: 'arcade',
         arcade: {
@@ -12,75 +92,8 @@ const config = {
             },
             debug: false
         }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-function preload() {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.spritesheet('touhou', 'assets/abigail.png', {
-        frameWidth: 16,
-        frameHeight: 32
-    });
-}
-
-function create() {
-    this.add.image(400, 300, 'sky');
-    const platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    
-    player = this.physics.add.sprite(100, 450, 'touhou').setScale(4);
-
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    player.body.setGravityY(300);
-
-    this.physics.add.collider(player, platforms);
-
-    cursors = this.input.keyboard.createCursorKeys();
-
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'touhou', frame: 0 }],
-        frameRate: 20
-    });
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('touhou', { start: 4, end: 7 }),
-        frameRate: 10,
-        repeat: -1
-    })
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('touhou', { start: 12, end: 15 }),
-        frameRate: 10,
-        repeate: -1
-    })
-}
-
-function update ()
-{
-    if (cursors.left.isDown) {
-        player.setVelocityX(-300);
-        player.anims.play('left', true);
-    } else if (cursors.right.isDown)
-    {
-        player.setVelocityX(300);
-        player.anims.play('right', true);
-    } else {
-        player.setVelocityX(0);
-        player.anims.play('turn');
-    }
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-500);
     }
 }
-
 
 const game = new Phaser.Game(config);
 
